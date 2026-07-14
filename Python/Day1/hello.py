332a5e4f092e251f8787cae283a17ab90fec9225
aman-kumar-10 <amank11645@gmail.com>
2026-07-14 16:38:15 +0000

    Day 2, getting started with python, variable and user inputs

---
+++ Python/Day1/hello.py
@@ -0,0 +1,7 @@
+print("Hi, welcome this is my first python program to print on the display.")
+print("\n\n")
+
+print("Hello Amn")
+print(" - We use print() just to display text on the screen.")
+print("\nThis is new line .")
+print(" '\n' is used to move in next line so \n here we are in the next line.")
